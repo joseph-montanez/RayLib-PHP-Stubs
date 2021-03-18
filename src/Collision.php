fef6c4a51cd8f01a54877b3aebb435143afca68e
Joseph Montanez <sutabi@gmail.com>
2021-03-17 20:20:05 -0700

    fixed to collision stub, added new RayLib checkLines

--- src/Collision.php
+++ src/Collision.php
@@ -3,18 +3,13 @@
 namespace raylib;
 
 
+/**
+ * Class Camera3D
+ * Basic shapes collision detection functions
+ * @package raylib
+ */
 class Collision
 {
-
-//    // Basic shapes collision detection functions
-//    RLAPI bool CheckCollisionRecs(Rectangle rec1, Rectangle rec2);                                           // Check collision between two rectangles
-//    RLAPI bool CheckCollisionCircles(Vector2 center1, float radius1, Vector2 center2, float radius2);        // Check collision between two circles
-//    RLAPI bool CheckCollisionCircleRec(Vector2 center, float radius, Rectangle rec);                         // Check collision between circle and rectangle
-//    RLAPI Rectangle GetCollisionRec(Rectangle rec1, Rectangle rec2);                                         // Get collision rectangle for two rectangles collision
-//    RLAPI bool CheckCollisionPointRec(Vector2 point, Rectangle rec);                                         // Check if point is inside rectangle
-//    RLAPI bool CheckCollisionPointCircle(Vector2 point, Vector2 center, float radius);                       // Check if point is inside circle
-//    RLAPI bool CheckCollisionPointTriangle(Vector2 point, Vector2 p1, Vector2 p2, Vector2 p3);               // Check if point is inside a triangle
-
 
     /**
      * Check collision between two rectangles
@@ -62,16 +57,76 @@ class Collision
     }
 
     /**
+     * Check if point is inside circle
+     *
+     * {@internal RLAPI bool CheckCollisionPointCircle(Vector2 point, Vector2 center, float radius); }}
+     *
+     * @param \raylib\Vector2 $point
+     * @param \raylib\Vector2 $center
+     * @param float           $radius
+     *
+     * @return bool
+     */
+    public static function checkPointCircle(Vector2 $point, Vector2 $center, float $radius): bool
+    {
+    }
+
+    /**
      * Check if point is inside rectangle
      *
-     * @param array $point The point to check
-     * @param array $rec   The rectangle to check
+     * @param \raylib\Vector2 $point The point to check
+     * @param \raylib\Rectangle $rec   The rectangle to check
      *
      * {@internal RLAPI bool CheckCollisionPointRec(Vector2 point, Rectangle rec) }}
      *
      * @return bool
      */
     public static function checkPointRec(Vector2 $point, Rectangle $rec): bool
+    {
+    }
+
+    /**
+     * Check if point is inside a triangle
+     *
+     * {@internal RLAPI bool CheckCollisionPointTriangle(Vector2 point, Vector2 p1, Vector2 p2, Vector2 p3); }}
+     *
+     * @param \raylib\Vector2 $point
+     * @param \raylib\Vector2 $p1
+     * @param \raylib\Vector2 $p2
+     * @param \raylib\Vector2 $p3
+     *
+     * @return bool
+     */
+    public static function checkPointTriangle(Vector2 $point, Vector2 $p1, Vector2 $p2, Vector2 $p3): bool
+    {
+    }
+
+    /**
+     * Check the collision between two lines defined by two points each, returns collision point by reference
+     *
+     * {@internal RLAPI bool CheckCollisionLines(Vector2 startPos1, Vector2 endPos1, Vector2 startPos2, Vector2 endPos2, Vector2 *collisionPoint); }}
+     *
+     * @param \raylib\Vector2 $startPos1
+     * @param \raylib\Vector2 $endPos1
+     * @param \raylib\Vector2 $startPos2
+     * @param \raylib\Vector2 $endPos2
+     * @param \raylib\Vector2 $collisionPoint
+     *
+     * @return bool
+     */
+    public static function checkLines(Vector2 $startPos1, Vector2 $endPos1, Vector2 $startPos2, Vector2 $endPos2, Vector2 $collisionPoint): bool
+    {
+    }
+
+    /**
+     * Get collision rectangle for two rectangles collision
+     *
+     * @param \raylib\Rectangle $rec1
+     * @param \raylib\Rectangle $rec2
+     *
+     * @return \raylib\Rectangle
+     */
+    public static function getRec(Rectangle $rec1, Rectangle $rec2): Rectangle
     {
     }
 }
